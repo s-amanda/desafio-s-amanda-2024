96e4b75c519ebf9e84641a98406f78503d6d625e
Amanda Santos <amandadsp3@gmail.com>
2024-09-10 15:48:05 -0300

    copia estrutura inicial do projeto

---
+++ src/recintos-zoo.js
@@ -0,0 +1,28 @@
+const recintos = [
+    { numero: 1, bioma: "savana", tamanhoTotal: 10, animaisExistentes: 3, especie: "macaco" },
+    { numero: 2, bioma: "floresta", tamanhoTotal: 5, animaisExistentes: 0, especie: null },
+    { numero: 3, bioma: "savana e rio", tamanhoTotal: 7, animaisExistentes: 1, especie: "gazela" },
+    { numero: 4, bioma: "rio", tamanhoTotal: 8, animaisExistentes: 0, especie: null },
+    { numero: 5, bioma: "savana", tamanhoTotal: 9, animaisExistentes: 1, especie: "leao" }
+]
+
+const animais = [
+    { especie: 'leao', espaco: 3, bioma: ['savana'] },
+    { especie: 'leopardo', espaco: 2, bioma: ['savana'] },
+    { especie: 'crocodilo', espaco: 3, bioma: ['rio'] },
+    { especie: 'macaco', espaco: 1, bioma: ['savana', 'floresta'] },
+    { especie: 'gazela', espaco: 2, bioma: ['savana'] },
+    { especie: 'hipopotamo', espaco: 4, bioma: ['savana', 'rio'] },
+]
+
+class RecintosZoo {
+
+    analisaRecintos(animal, quantidade) {
+
+
+
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
